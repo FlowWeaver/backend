50e364115f9b9f5879ba6545fb5db1917c9b5fb0
Yousung Jung <jys01012@gmail.com>
2025-09-29 18:40:48 +0900

    Task io data 조회 api (#235)
    * chore: gitingore에 key, blogger 설정 추가
    
    * feat: Task io data api

--- apps/user-service/src/main/java/site/icebang/domain/workflow/mapper/TaskIoDataMapper.java
+++ apps/user-service/src/main/java/site/icebang/domain/workflow/mapper/TaskIoDataMapper.java
@@ -1,8 +1,10 @@
 package site.icebang.domain.workflow.mapper;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import site.icebang.domain.workflow.model.TaskIoData;
 
@@ -11,4 +13,9 @@ public interface TaskIoDataMapper {
   void insert(TaskIoData taskIoData);
 
   Optional<TaskIoData> findOutputByTaskRunId(Long taskRunId);
+
+  List<TaskIoData> findByTaskRunIds(
+      @Param("taskRunIds") List<Long> taskRunIds,
+      @Param("ioType") String ioType,
+      @Param("limit") Integer limit);
 }
