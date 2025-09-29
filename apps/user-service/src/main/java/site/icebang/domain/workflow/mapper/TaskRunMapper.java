6cf528f2cd045ba15d5ea803cc3a0ea04c993819
jihukimme <jihu0210@naver.com>
2025-09-29 11:28:20 +0900

    feat: TaskRun관련 Mapper 추가

--- apps/user-service/src/main/java/site/icebang/domain/workflow/mapper/TaskRunMapper.java
+++ apps/user-service/src/main/java/site/icebang/domain/workflow/mapper/TaskRunMapper.java
@@ -4,9 +4,13 @@ import org.apache.ibatis.annotations.Mapper;
 
 import site.icebang.domain.workflow.model.TaskRun;
 
+import java.util.Optional;
+
 @Mapper
 public interface TaskRunMapper {
   void insert(TaskRun taskRun);
 
   void update(TaskRun taskRun);
+
+  Optional<TaskRun> findLatestSuccessRunInJob(Long jobRunId, String taskName);
 }
